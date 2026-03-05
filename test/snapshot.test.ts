dd8ee43037d3a465fc4fc43a68b787f33f51b44d
Andre Kurait <andrekurait@gmail.com>
2026-03-05 00:20:28 +0000

    test: add snapshot tests for CFN template stability
    - NetworkStack, OpenSearchDomainStack, ServerlessCollectionStack snapshots
    - Any template change from CDK upgrades or refactors requires explicit --updateSnapshot
    - Prevents accidental breaking changes to deployed infrastructure
    
    Signed-off-by: Andre Kurait <andrekurait@gmail.com>

---
+++ test/snapshot.test.ts
@@ -0,0 +1,54 @@
+import { Template } from "aws-cdk-lib/assertions";
+import { createStackComposer } from "./test-utils";
+import { describe, test, expect } from '@jest/globals';
+import { NetworkStack } from "../lib/network-stack";
+import { OpenSearchDomainStack } from "../lib/opensearch-domain-stack";
+import { ServerlessCollectionStack } from "../lib/serverless-collection-stack";
+import { ClusterType } from "../lib/components/common-utilities";
+
+describe('Snapshot Tests', () => {
+
+  test('NetworkStack template snapshot', () => {
+    const stackComposer = createStackComposer({
+      clusters: [{
+        clusterId: "snap",
+        clusterType: ClusterType.OPENSEARCH_MANAGED_SERVICE,
+      }]
+    });
+    const networkStack = stackComposer.stacks.find(s => s instanceof NetworkStack)!;
+    expect(Template.fromStack(networkStack).toJSON()).toMatchSnapshot();
+  });
+
+  test('OpenSearchDomainStack template snapshot', () => {
+    const stackComposer = createStackComposer({
+      clusters: [{
+        clusterId: "snap",
+        clusterType: ClusterType.OPENSEARCH_MANAGED_SERVICE,
+        dataNodeType: "r6g.large.search",
+        dataNodeCount: 2,
+        ebsEnabled: true,
+        ebsVolumeSize: 100,
+        ebsVolumeType: "GP3",
+        nodeToNodeEncryptionEnabled: true,
+        enforceHTTPS: true,
+      }]
+    });
+    const domainStack = stackComposer.stacks.find(s => s instanceof OpenSearchDomainStack)!;
+    expect(Template.fromStack(domainStack).toJSON()).toMatchSnapshot();
+  });
+
+  test('ServerlessCollectionStack template snapshot', () => {
+    const stackComposer = createStackComposer({
+      clusters: [{
+        clusterId: "snap",
+        clusterType: ClusterType.OPENSEARCH_SERVERLESS,
+        collectionType: "SEARCH",
+        standbyReplicas: "DISABLED",
+        domainRemovalPolicy: "DESTROY",
+      }]
+    });
+    const serverlessStack = stackComposer.stacks.find(s => s instanceof ServerlessCollectionStack)!;
+    expect(Template.fromStack(serverlessStack).toJSON()).toMatchSnapshot();
+  });
+
+});
